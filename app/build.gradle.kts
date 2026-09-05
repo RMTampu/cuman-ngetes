10b4b08d83b7ca5c861846bb9b25350071d1da57
RMTampu <rikcyrish@gmail.com>
2026-09-05 17:30:07 +0700

    Bump diagnostic build to 0.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.trafficmarker"
         minSdk = 23
         targetSdk = 30
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.2.1"
         ndk {
             abiFilters += listOf("arm64-v8a")
         }
