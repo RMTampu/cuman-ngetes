0dbfebcee1505108c392c5bf7710a847f88acf60
RMTampu <rikcyrish@gmail.com>
2026-09-05 16:11:33 +0700

    Package Android 11 arm64 test APK only

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,6 +13,9 @@ android {
         targetSdk = 30
         versionCode = 1
         versionName = "0.1.0"
+        ndk {
+            abiFilters += listOf("arm64-v8a")
+        }
     }
 
     buildTypes {
