8be6cef94e230b10b4c2100356fbf582be842a50
RMTampu <rikcyrish@gmail.com>
2026-09-05 17:57:02 +0700

    Bump marker backup build to 0.2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.trafficmarker"
         minSdk = 23
         targetSdk = 30
-        versionCode = 3
-        versionName = "0.2.1"
+        versionCode = 4
+        versionName = "0.2.2"
         ndk {
             abiFilters += listOf("arm64-v8a")
         }
