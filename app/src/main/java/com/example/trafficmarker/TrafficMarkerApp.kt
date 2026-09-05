5a94da77eef3e514b1b837c6997e6d24e7eaaa60
RMTampu <rikcyrish@gmail.com>
2026-09-05 15:56:08 +0700

    Add app/src/main/java/com/example/trafficmarker/TrafficMarkerApp.kt

---
+++ app/src/main/java/com/example/trafficmarker/TrafficMarkerApp.kt
@@ -0,0 +1,20 @@
+package com.example.trafficmarker
+
+import android.app.Application
+import com.example.trafficmarker.store.AlertManager
+import com.example.trafficmarker.store.MarkerStore
+
+class TrafficMarkerApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        AppContext.init(this)
+        MarkerStore.init(this)
+        AlertManager.init(this)
+    }
+}
+
+object AppContext {
+    lateinit var app: Application
+        private set
+    fun init(application: Application) { app = application }
+}
